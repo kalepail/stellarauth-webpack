e0cde3922d9943f1e1a4b186de08928eba7158a0
Tyler van der Hoeven <hi@tyvdh.com>
2018-05-11 15:20:18 -0400

    stellarauth

--- webpack-utils/common-paths.js
+++ webpack-utils/common-paths.js
@@ -2,5 +2,5 @@ const path = require('path');
 
 module.exports = {
   outputPath: path.resolve(__dirname, '../', 'dist'),
-  publicPath: 'https://stellar-auth-example.gly.sh/'
+  publicPath: 'https://stellarauth.gly.sh/'
 }
